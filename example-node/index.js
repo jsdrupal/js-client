a1ad00594b82f7de4c3b16b3b1c405ba5f08224e
Sally Young <sally@justafish.co.uk>
2020-12-03 17:31:42 +0000

    Update index.js

--- example-node/index.js
+++ example-node/index.js
@@ -2,7 +2,7 @@ const request = require('@drupal/client').request;
 
 module.exports = async () => {
   const resp = await request({
-    base: 'https://cms.contentacms.io/api',
+      base: 'https://cms.contentacms.io/api',
     entity: 'menus',
   });
   return resp;

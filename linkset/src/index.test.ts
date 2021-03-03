69f436c19238d8d1006b9db4c71979eb1c1e6734
Gabriel SULLICE <gabriel@sullice.com>
2021-03-03 10:37:18 -0500

    Add registered target attribute denormalization test.

--- linkset/src/index.test.ts
+++ linkset/src/index.test.ts
@@ -2,6 +2,8 @@ import { normalize, denormalize } from './index';
 
 const figure4 = `{"linkset":[{"anchor":"https://example.org/article/view/7507","author":[{"href":"https://orcid.org/0000-0002-1825-0097"}],"item":[{"href":"https://example.org/article/7507/item/1","type":"application/pdf"},{"href":"https://example.org/article/7507/item/2","type":"text/csv"}],"cite-as":[{"href":"https://doi.org/10.5555/12345680","title":"AMethodologyfortheEmulationofArchitecture"}]},{"anchor":"https://example.com/links/article/7507","alternate":[{"href":"https://mirror.example.com/links/article/7507","type":"application/linkset"}]}]}`;
 
+const section_4_2_4_1 =`{"linkset":[{"anchor":"http://example.net/bar","next":[{"href":"http://example.com/foo","type":"text/html","hreflang":["en","de"]}]}]}`;
+
 test('normalize()', () => {
   const linkset = denormalize(JSON.parse(figure4));
   const json = JSON.stringify(normalize(linkset));
@@ -34,6 +36,18 @@ describe('denormalize()', () => {
     expect(items[0].attributes.type).toBe('application/pdf');
     expect(items[1].href).toBe('https://example.org/article/7507/item/2');
     expect(items[1].attributes.type).toBe('text/csv');
+  });
+
+  it('should be able to denormalize registered target attributes', () => {
+    const actual = denormalize(JSON.parse(section_4_2_4_1));
+    expect(actual.elements.length).toBe(1);
+    const link = actual.elements[0];
+    expect(link.anchor).toBe('http://example.net/bar');
+    expect(link.rel).toBe('next');
+    expect(link.href).toBe('http://example.com/foo');
+    expect(link.attributes.type).toBe('text/html');
+    expect(link.attributes.hreflang[0]).toBe('en');
+    expect(link.attributes.hreflang[1]).toBe('de');
   });
 });
 
